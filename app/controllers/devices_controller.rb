6c8c8262d1e552431d2cde6115dcf1177e7fd2a4
roger <komodoods@gmail.com>
2025-03-22 23:54:37 +0100

    [FEATURES-17] - improved errors for unassigning devices, minor changes to controller assign method in accordance to existing tests

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,18 +3,26 @@
 class DevicesController < ApplicationController
   before_action :authenticate_user!, only: %i[assign unassign]
   def assign
-    AssignDeviceToUser.new(
-      requesting_user: @current_user,
-      serial_number: params[:serial_number],
-      new_device_owner_id: params[:new_device_owner_id]
-    ).call
-    head :ok
+    begin
+      AssignDeviceToUser.new(
+        requesting_user: @current_user,
+        serial_number: params[:device][:serial_number],
+        new_device_owner_id: params[:new_owner_id]
+      ).call
+      head :ok
+    rescue StandardError => e
+      render json: { error: 'Unauthorized' }, status: 422
+    end
   end
 
   def unassign
-    ReturnDeviceFromUser.new(user: @current_user, serial_number: params[:serial_number], from_user: params[:user_id])
-                        .call
-    head :ok
+    begin
+      ReturnDeviceFromUser.new(user: @current_user, serial_number: params[:serial_number], from_user: params[:user_id])
+      .call
+      head :ok
+    rescue StandardError => e
+      head :unprocessable_entity
+    end
   end
 
   private
--- app/errors/unassigning_error.rb
+++ app/errors/unassigning_error.rb
@@ -1,12 +1,18 @@
 module UnassigningError
   class OwnedByAnotherUser < StandardError
-    def initialize(message = "You are not authorized to perform this action.")
+    def initialize(message = "You are cannot unassign a device from another user!.")
       super(message)
     end
   end
 
   class DeviceNotRented < StandardError
-    def initialize(message = "Device hasn't been rented by this user.")
+    def initialize(message = "Device hasn't been rented by this user!")
+      super(message)
+    end
+  end
+
+  class DeviceWasAlreadyRentedByThisUser < StandardError
+    def initialize(message = "You cannot reassign a device you've already rented!.")
       super(message)
     end
   end
